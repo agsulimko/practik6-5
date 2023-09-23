80aecf7780b2697dafe5808fc806dbe0275cc9cb
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-09-23 15:01:24 +0300

    second task

--- src/components/WordsForm/WordsForm.jsx
+++ src/components/WordsForm/WordsForm.jsx
@@ -1,53 +1,62 @@
-import React, { Component } from 'react';
+import { useReducer } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import css from './WordsForm.module.css';
 import { nanoid } from 'nanoid';
+const initialState = {
+  ukWord: '',
+  enWord: '',
+};
 
-export default class WordsForm extends Component {
-  state = {
-    ukWord: '',
-    enWord: '',
-  };
-
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit({ ...this.state, id: nanoid(5) });
-    this.setState({
-      ukWord: '',
-      enWord: '',
-    });
-  };
-
-  render() {
-    return (
-      <div className={css.wordsFormWrapper}>
-        <form className={css.wordsForm} onSubmit={this.handleSubmit}>
-          <TextField
-            onChange={this.handleChange}
-            id="outlined-basic"
-            label="Ukrainian"
-            variant="outlined"
-            value={this.state.ukWord}
-            name="ukWord"
-          />
-          <TextField
-            onChange={this.handleChange}
-            id="outlined-basic"
-            label="English"
-            variant="outlined"
-            value={this.state.enWord}
-            name="enWord"
-          />
-          <Button variant="contained" type="submit">
-            Add
-          </Button>
-        </form>
-      </div>
-    );
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'change':
+      return { ...state, [action.payload.name]: action.payload.value };
+    case 'reset':
+      return initialState;
+    default:
+      return state;
   }
-}
+};
+const WordsForm = ({ onSubmit }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleChange = ({ target: { name, value } }) => {
+    // this.setState({ [name]: value });
+    dispatch({ type: 'change', payload: { name, value } });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit({ ...state, id: nanoid(5) });
+    dispatch({ type: 'reset' });
+  };
+
+  return (
+    <div className={css.wordsFormWrapper}>
+      <form className={css.wordsForm} onSubmit={handleSubmit}>
+        <TextField
+          onChange={handleChange}
+          id="outlined-basic"
+          label="Ukrainian"
+          variant="outlined"
+          value={state.ukWord}
+          name="ukWord"
+        />
+        <TextField
+          onChange={handleChange}
+          id="outlined-basic"
+          label="English"
+          variant="outlined"
+          value={state.enWord}
+          name="enWord"
+        />
+        <Button variant="contained" type="submit">
+          Add
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default WordsForm;
